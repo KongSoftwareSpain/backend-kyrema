2790593f0f3f81b688787dfaec002cb9a0ad6438
PericoMar <pericomarytb@gmail.com>
2024-10-29 14:03:33 +0100

    Refactor PolizaController to handle file uploads for documents

--- app/Http/Controllers/PolizaController.php
+++ app/Http/Controllers/PolizaController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Poliza;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
 
 class PolizaController extends Controller
 {
@@ -20,45 +22,58 @@ class PolizaController extends Controller
     }
 
     public function store(Request $request)
-{
-    // Validación de los campos
-    $request->validate([
-        'compania_id' => 'required',
-        'numero' => 'required',
-        'ramo' => 'required',
-        'descripcion' => 'nullable',
-        'fecha_inicio' => 'nullable',
-        'fecha_fin_venta' => 'nullable',
-        'fecha_fin_servicio' => 'nullable',
-        'prima_neta' => 'nullable',
-        'comision' => 'nullable',
-        'estado' => 'nullable',
-        'observaciones' => 'nullable',
-        'doc_adjuntos_1' => 'nullable',
-        'doc_adjuntos_2' => 'nullable|file|mimes:pdf,doc,docx',
-        'doc_adjuntos_3' => 'nullable|file|mimes:pdf,doc,docx',
-        'doc_adjuntos_4' => 'nullable|file|mimes:pdf,doc,docx',
-        'doc_adjuntos_5' => 'nullable|file|mimes:pdf,doc,docx',
-        'doc_adjuntos_6' => 'nullable|file|mimes:pdf,doc,docx',
-    ]);
+    {
+        // Validación de los campos
+        $request->validate([
+            'compania_id' => 'required',
+            'numero' => 'required',
+            'ramo' => 'required',
+            'descripcion' => 'nullable',
+            'fecha_inicio' => 'nullable',
+            'fecha_fin_venta' => 'nullable',
+            'fecha_fin_servicio' => 'nullable',
+            'prima_neta' => 'nullable',
+            'comision' => 'nullable',
+            'estado' => 'nullable',
+            'observaciones' => 'nullable',
+            'doc_adjuntos_1' => 'nullable',
+            'doc_adjuntos_2' => 'nullable',
+            'doc_adjuntos_3' => 'nullable',
+            'doc_adjuntos_4' => 'nullable',
+            'doc_adjuntos_5' => 'nullable',
+            'doc_adjuntos_6' => 'nullable',
+        ]);
 
-    // Crear una nueva póliza sin los documentos primero
-    $polizaData = $request->except(['doc_adjuntos_1', 'doc_adjuntos_2', 'doc_adjuntos_3', 'doc_adjuntos_4', 'doc_adjuntos_5', 'doc_adjuntos_6']);
-    $poliza = Poliza::create($polizaData);
+        Log::info($request->all());
 
-    // Guardar cada archivo adjunto si está presente
-    for ($i = 1; $i <= 6; $i++) {
-        $docField = "doc_adjuntos_$i";
-        if ($request->hasFile($docField)) {
-            $filePath = $request->file($docField)->store('public/docs');
-            $poliza->$docField = basename($filePath); // Almacena el nombre del archivo
+        // Crear una nueva póliza sin los documentos primero
+        $polizaData = $request->except(['doc_adjuntos_1', 'doc_adjuntos_2', 'doc_adjuntos_3', 'doc_adjuntos_4', 'doc_adjuntos_5', 'doc_adjuntos_6']);
+        $poliza = Poliza::create($polizaData);
+
+        // Guardar cada archivo adjunto si está presente
+        for ($i = 1; $i <= 6; $i++) {
+            $docField = "doc_adjuntos_$i";
+            if ($request->hasFile($docField)) {
+                $doc = $request->file($docField);
+                $nombreArchivo = $doc->getClientOriginalName();
+                $rutaArchivo = 'docs/' . $nombreArchivo;
+
+                // Comprobar si ya existe un archivo con el mismo nombre
+                if (Storage::disk('public')->exists($rutaArchivo)) {
+                    return response()->json(['error' => 'Ya existe un document con el nombre '. $nombreArchivo], 400);
+                }
+
+                // Guardar la plantilla Excel en el sistema de archivos
+                Storage::disk('public')->putFileAs('docs', $doc, $nombreArchivo);
+            } else {
+                Log::info("No se encontró el archivo $docField");
+            }
         }
+
+        $poliza->save();
+
+        return response()->json($poliza);
     }
-
-    $poliza->save();
-
-    return response()->json($poliza);
-}
 
 public function update(Request $request, $id)
 {
